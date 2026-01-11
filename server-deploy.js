4071322e82c038e1266244480cdd0295917b01c0
1429482212aa-ops <1429482212aa@gmail.com>
2026-01-11 20:16:06 +0800

    Add app ready message for Railway deployment

--- server-deploy.js
+++ server-deploy.js
@@ -258,4 +258,5 @@ server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Server listening on 0.0.0.0:${PORT}`);
   console.log('WebSocket server ready to accept connections');
+  console.log('App is ready for connections');
 });
